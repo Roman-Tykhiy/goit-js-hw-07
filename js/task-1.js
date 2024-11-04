ba08cd6030e08205cabefc60b831fd48bf4a8e62
Roman_Tykhiy <2rstykhiy@gmail.com>
2024-11-04 05:56:57 +0200

    hw7-1

--- js/task-1.js
+++ js/task-1.js
@@ -6,6 +6,9 @@ const catItem = document.querySelectorAll(".item");
 
 
 catUl.forEach(element => console.log(`Numbers of categories: ${element.children.length}`));
-catItem.forEach(element => console.log(`Category: ${element.children[0].textContent}`,
-    `Element: ${element.children[1].children.length}`));
+catItem.forEach(element => (
+    console.log(`Category: ${element.children[0].textContent}`),
+    console.log( `Element: ${element.children[1].children.length}`)
+    
+));
 
